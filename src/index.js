732ee6139c49c77ac0e27f57e6b615dcb96d7179
vRP.Combo <gabrielfleig04@gmail.com>
2021-10-14 14:14:50 -0300

    adding webhook link now, and compress file dump

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const { Client, Intents } = require('discord.js')
+const { WebhookClient } = require('discord.js')
+const { webhook, dump } = require('./config/config.json')
+const { createGzip } = require('zlib')
 const fs = require('fs')
-const dump = require('mysqldump')
-const config = require('./config/config.json')
-const app = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+const client = new WebhookClient({ url: webhook })
+const mysqldump = require('mysqldump')
+const del = require('del')
 
-async function executeDump(){
-    if(!config.dump || typeof config.dump != 'object' || !config.dump.host  || !config.dump.database || !config.dump.user){
+const executeDump = async () => {
+    if(!dump || typeof dump != 'object' || !dump.host  || !dump.database || !dump.user){
         console.error('[SERVER] Algo deu errado, você definiu de forma incorreta sua config.json')
         return
     }
@@ -13,44 +15,55 @@ async function executeDump(){
     const folder = './cache'
     const fileName = `dump-${date.getDate() < 10 ? '0'+date.getDate() : date.getDate()}-${(date.getMonth() + 1) < 10 ? '0'+(date.getMonth() + 1) : date.getMonth() + 1}-${date.getFullYear()}_${date.getHours() < 10 ? '0'+date.getHours() : date.getHours()}-${date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes()}-${date.getSeconds() < 10 ? '0'+date.getSeconds() : date.getSeconds()}.sql`
     const fileDir = `${folder}/${fileName}`
+    const fileZip = fileDir+'.gz'
     if (!fs.existsSync(folder)){
         fs.mkdirSync(folder)
     }
-    await dump({
-        connection: {
-            host: config.dump.host,
-            user: config.dump.user,
-            password: config.dump.password,
-            database: config.dump.database,
-        },
-        dumpToFile: fileDir,
-    }).catch(() => {})
-    if (!fs.existsSync(fileDir)){
+    try {
+        await mysqldump({
+            connection: {
+                host: dump.host,
+                user: dump.user,
+                password: dump.password,
+                database: dump.database,
+            },
+            dumpToFile: fileDir
+        })
+    } catch (err) {
+        console.log('[ERRO] Ao criar o arquivo.')
         return false
     }
-    return fileDir
+    const zip = createGzip()
+
+    const read = fs.createReadStream(fileDir)
+    const write = fs.createWriteStream(fileZip)
+    read.pipe(zip).pipe(write)
+    await new Promise(r => setTimeout(r,1000))
+    del(fileDir).catch(() => {})
+
+    return fileZip
 }
 
-async function taskDump(){
-    if(!config.dump || typeof config.dump != 'object' || !config.dump.channelId || !config.dump.database){
+const taskDump = async () => {
+    if(!dump || typeof dump != 'object' || !dump.database){
         console.error('[SERVER] Algo deu errado, você definiu de forma incorreta sua config.json')
         return
     } 
     const fileDir = await executeDump()
     if(fileDir){
-        const channelDump = await app.channels.fetch(config.dump.channelId).catch(() => {})
-        const send = await channelDump.send({
-            content: '```ini\n[DATABASE '+config.dump.database.toUpperCase()+']: DUMP AUTOMÁTICO\n[DATA]: '+String(new Date())+' ```',
-            files: [fileDir]
-        }).catch(() => {})
+        try {
+            await client.send({
+                content: '```ini\n[DATABASE '+dump.database.toUpperCase()+']: DUMP AUTOMÁTICO\n[DATA]: '+String(new Date())+' ```',
+                files: [String(fileDir)]
+            })
+        } catch (err) {
+            console.log(err)
+            console.log('[ERRO] Ao enviar o arquivo para o webhook.')
+        }
     }
-    setTimeout(taskDump, (config.dump.cooldown || 10) * 60 * 1000)
+    console.log(fileDir)
+    setTimeout(taskDump, (dump.cooldown || 10) * 60 * 1000)
 }
 
-
-app.login(config.token).then(err => {
-    if(!err)return
-    console.log(`[SERVER] Iniciado em ${app.guilds.cache.size} servidores.`)
-
-    setTimeout(taskDump, (config.dump.cooldown || 10) * 60 * 1000)
-})
+setTimeout(taskDump, (dump.cooldown || 10) * 1000)
+console.log('[SERVER] Webhook conectado!')
